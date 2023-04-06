c4b67ec1ee456ff42253ab3a3a550ff36c0133ae
Forestuss <guillaume.quertain@gmail.com>
2023-04-06 22:54:35 +0200

    Ajout Son Script anim

--- HUB_SemaineThematique/Assets/Scripts/anim.cs
+++ HUB_SemaineThematique/Assets/Scripts/anim.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class anim : MonoBehaviour
 {
     [SerializeField] private Animator animator;
-    // [SerializeField] private AudioSource audioSource;
-    // [SerializeField] private AudioClip soundOnTriggerEnter;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip soundOnTriggerEnter;
 
 
     private void OnTriggerEnter(Collider other)
     {
         animator.Play("a_talking");
         transform.LookAt(other.transform);
-        //audioSource.clip = soundOnTriggerEnter;
-        //audioSource.Play();
+        audioSource.clip = soundOnTriggerEnter;
+        audioSource.Play();
     }
 
     private void OnTriggerExit(Collider other)
     {
-        //audioSource.Stop();
+        audioSource.Stop();
         animator.Play("a_idle");
     }
 }
